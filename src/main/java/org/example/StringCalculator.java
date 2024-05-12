fb3d43e9afc4f5bfe8b4125f824259715d9d7c28
Sathwik <sathwikreddy101@gmail.com>
2024-05-12 10:19:48 +0530

    adding code for adding two numbers

--- src/main/java/org/example/StringCalculator.java
+++ src/main/java/org/example/StringCalculator.java
@@ -3,8 +3,14 @@ package org.example;
 public class StringCalculator {
     public static int add(String input){
         if(!input.isEmpty()){
-            // no code change needed this part of code automatically throws exception
-            return Integer.parseInt(input);
+            if(input.length() == 1){
+                return Integer.parseInt(input);
+            }
+            int sum = 0;
+            String[] numbers = input.split(",");
+            sum += Integer.parseInt(numbers[0].trim());
+            sum += Integer.parseInt(numbers[1].trim());
+            return sum;
         }
         return 0;
     }
