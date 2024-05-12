52e7f9955dbc1e06e545fc31fbaa0fb68e215229
Sathwik <sathwikreddy101@gmail.com>
2024-05-12 09:54:06 +0530

    adding code for valid single integer string as input case

--- src/main/java/org/example/StringCalculator.java
+++ src/main/java/org/example/StringCalculator.java
@@ -2,6 +2,9 @@ package org.example;
 
 public class StringCalculator {
     public static int add(String input){
+        if(!input.isEmpty()){
+            return Integer.parseInt(input);
+        }
         return 0;
     }
 }
