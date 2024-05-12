f57e866ec70de1dafd6b854aca01652104b90e3c
Sathwik <sathwikreddy101@gmail.com>
2024-05-12 10:57:01 +0530

    refactoring added code for sum of two numbers

--- src/main/java/org/example/StringCalculator.java
+++ src/main/java/org/example/StringCalculator.java
@@ -3,15 +3,19 @@ package org.example;
 public class StringCalculator {
     public static int add(String input){
         if(!input.isEmpty()){
-            if(input.length() == 1){
-                return Integer.parseInt(input);
-            }
-            int sum = 0;
-            String[] numbers = input.split(",");
-            sum += Integer.parseInt(numbers[0].trim());
-            sum += Integer.parseInt(numbers[1].trim());
-            return sum;
+            return sum(input);
         }
         return 0;
+    }
+
+    private static int sum(String input){
+        if(input.length() == 1){
+            return Integer.parseInt(input);
+        }
+        int sum = 0;
+        String[] numbers = input.split(",");
+        sum += Integer.parseInt(numbers[0].trim());
+        sum += Integer.parseInt(numbers[1].trim());
+        return sum;
     }
 }
