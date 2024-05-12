4ee9781c7f1a7cee4e9a122fa1e4d85082843beb
Sathwik <sathwikreddy101@gmail.com>
2024-05-12 10:04:16 +0530

    test case to handle invalid character in single number input

--- src/main/java/org/example/StringCalculator.java
+++ src/main/java/org/example/StringCalculator.java
@@ -3,6 +3,7 @@ package org.example;
 public class StringCalculator {
     public static int add(String input){
         if(!input.isEmpty()){
+            // no code change needed this part of code automatically throws exception
             return Integer.parseInt(input);
         }
         return 0;
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -1,4 +1,5 @@
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.example.StringCalculator;
 import org.junit.Test;
@@ -16,6 +17,12 @@ public class StringCalculatorTest {
     public void should_return_number_for_one_number() {
         // a single character string with valid integer should return same integer as output
         assertThat(StringCalculator.add("1")).isEqualTo(1);
+    }
+
+    @Test
+    public void should_return_exception_for_invalid_one_number() {
+        // a single character string with invalid character i.e other than numbers should return exception
+        assertThatThrownBy(() -> StringCalculator.add("$")).isInstanceOf(NumberFormatException.class);
     }
 
 }
