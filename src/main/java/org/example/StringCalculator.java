fa9877cef5091c07ac2c2b584edf14944c2f24dd
Sathwik <sathwikreddy101@gmail.com>
2024-05-12 12:14:30 +0530

    added and refactored code for comma and new line it is invalid

--- src/main/java/org/example/StringCalculator.java
+++ src/main/java/org/example/StringCalculator.java
@@ -12,11 +12,20 @@ public class StringCalculator {
     }
 
     private static int sum(String input){
-        List<Integer> numbers = Arrays.stream(input.split(",|\n"))
-                .map(Integer::parseInt)
-                .toList();
+        List<Integer> numbers = getIntegerListFromString(split(input));
         return numbers.stream()
                 .reduce(Integer::sum)
                 .orElseThrow();
+    }
+
+    private static List<Integer> getIntegerListFromString(String[] strings) {
+        return Arrays.stream(strings)
+                .filter(str -> !str.isEmpty())
+                .map(Integer::parseInt)
+                .toList();
+    }
+
+    private static String[] split(String input) {
+        return input.split("(?<!,)\\s*(,|\\n)");
     }
 }
